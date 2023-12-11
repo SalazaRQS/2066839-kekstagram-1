181c8e64b5ac615510015ecc8b41aa123ba58709
Vladimir <salazar888@mail.ru>
2023-12-11 21:19:23 +0300

    Нужно больше функций

--- js/function.js
+++ js/function.js
@@ -1,21 +1,64 @@
-
-// Функция палиндром
+// 1. Функция палиндром
 
 function checkPalindrome(string) {
   string = string.toLowerCase().replace(/\s/g, '');
   return string === string.split('').reverse().join('');
 }
 
-// console.log(checkPalindrome('Довод'));
+checkPalindrome();
 
-checkPalindrome('Довод');
-
-// Функция извлечения чисел
+// 2. Функция извлечения чисел
 
 function getNumber(string) {
-  return string.replace(/\D/g, '');
+  if (string === Number(string)) {
+    return Number(string);
+  } else {
+    return string.replace(/\D/g, '');
+  }
 }
 
-getNumber('1 кефир');
+getNumber();
 
-// console.log(getNumber('А  9 я томат 1'));
+// 3. Функция, которая принимает три параметра
+
+function replaceString(str, length, addSymbol) {
+  for (let i = 0; i < length; i++) {
+    addSymbol = addSymbol.slice(0);
+    if (i === length) {
+      break;
+    }
+  } for (let i = 0; i < length; i++) {
+    str = addSymbol.slice(0, length - str.length) + str;
+    if (i === length) {
+      break;
+    }
+  }
+  return str;
+}
+
+replaceString();
+
+// console.log(replaceString('1', 4, '0'));
+// console.log(replaceString('q', 4, 'werty'));
+// console.log(replaceString('1', 2, '0'));
+// console.log(replaceString('q', 4, 'we'));
+// console.log(replaceString('qwerty', 4, '0'));
+
+// 4. Функция для проверки длины строки.
+
+function getLength(string, maxLingth) {
+  let answer;
+  if (string.length <= maxLingth) {
+    answer = true;
+  } else if (string.length > maxLingth) {
+    answer = false;
+  } else {
+    answer = false;
+  }
+  return answer;
+}
+
+
+// console.log(getLength('проверяемая строка', 20));
+
+getLength();
